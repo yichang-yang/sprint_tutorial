5134472dfd1f41f9befbc9144b0f938c61d086dd
Steven C. Howell <steven.howell@nist.gov>
2016-07-16 10:15:05 -0500

    added my_root test and another my_sum test

--- sprint_tutorial/tests/test_compute.py
+++ sprint_tutorial/tests/test_compute.py
@@ -7,8 +7,14 @@ import pytest
 def test_my_sum():
     assert my_sum(0, 0) == 0
     assert my_sum(3, 0) == 3
+    assert my_sum(99, 1) == 100
 
 
 def test_my_pow():
     assert my_power(2, 4) == 16
     assert my_power(5, 2) == 25
+
+def test_my_root():
+    assert my_root(4) == 2.0
+    assert my_power(8, 2) == 2.0
+    
